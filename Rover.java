c62487f205a147d7dac1b0aa1a843b4d7fff194c
Eduardo Schweitzer <eduardo.schweitzer@acad.pucrs.br>
2018-03-21 17:32:26 -0300

    Versao final

--- Rover.java
+++ Rover.java
@@ -1,3 +1,5 @@
+import java.util.InputMismatchException;
+import java.util.StringTokenizer;
 
 public class Rover {
 
@@ -6,15 +8,20 @@ public class Rover {
 	private int posX;
 	private int posY;
 
-	public Rover(int x, int y, String dir, Plateau p) {
-		posX = x;
-		posY = y;
-		facinDir = dir;
+	public Rover(String position, Plateau p) {
+
+		if (position.matches("-?\\d+ " + "-?\\d+ " + "[A-Za-z]")) {
+			StringTokenizer st = new StringTokenizer(position, " ");
+			this.posX = Integer.parseInt(st.nextToken());
+			this.posY = Integer.parseInt(st.nextToken());
+			this.facinDir = st.nextToken();
+		} else
+			throw new InputMismatchException("Formato Invalido");
+
 		this.p = p;
-		p.updatePosition(x + " " + y);
+		p.updatePosition(posX + " " + posY);
 
 	}
-
 	public boolean updateX(int value) {
 		if (value != 1 || value != -1)
 			return false;
@@ -44,16 +51,16 @@ public class Rover {
 			char c = mov.charAt(index);
 			String currentPos = posX + " " + posY;
 			if (c == 'M') {
-				if (facinDir == "N")
+				if (facinDir.matches("N"))
 					if(posY+1>p.getYMax()) return false;
 					else nposY = posY + 1;
-				else if (facinDir == "S")
+				else if (facinDir.matches("S"))
 					if(posY-1<0) return false;
 					else nposY = posY - 1;
-				else if (facinDir == "E")
+				else if (facinDir.matches("E"))
 					if(posX+1>p.getXMax()) return false;
 					else nposX = posX + 1;
-				else if (facinDir == "W")
+				else if (facinDir.matches("W"))
 					if(posX-1<0) return false;
 					else nposX = posX - 1;
 				
@@ -65,22 +72,22 @@ public class Rover {
 					
 				} 
 			} else if (c == 'L') {
-				if (facinDir == "N")
+				if (facinDir.matches("N"))
 					nDir = "W";
-				if (facinDir == "W")
+				if (facinDir.matches("W"))
 					nDir = "S";
-				if (facinDir == "S")
+				if (facinDir.matches("S"))
 					nDir = "E";
-				if (facinDir == "E")
+				if (facinDir.matches("E"))
 					nDir = "N";
 			} else if (c == 'R') {
-				if (facinDir == "N")
+				if (facinDir.matches("N"))
 					nDir = "E";
-				if (facinDir == "E")
+				if (facinDir.matches("E"))
 					nDir = "S";
-				if (facinDir == "S")
+				if (facinDir.matches("S"))
 					nDir = "W";
-				if (facinDir == "W")
+				if (facinDir.matches("W"))
 					nDir = "N";
 			} else return false;
 			facinDir = nDir;
